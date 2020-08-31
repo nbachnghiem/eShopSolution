735a7c7298529094151680ab81518ae9efbfadc5
Nghiem Nguyen <nbachnghiem97@gmail.com>
2020-08-31 17:33:35 +0700

    db_design

---
+++ eShopSolutionData/Entities/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+namespace eShopSolutionData.Entities
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        public string Description { get; set; }
+    }
+}
---
+++ eShopSolutionData/Enums/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolutionData.Enums
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
